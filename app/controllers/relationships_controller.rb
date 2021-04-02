6cf1140122915c6bf6a17a5c88fdd60f9c450030
EC2 Default User <ec2-user@ip-172-31-20-15.ap-northeast-1.compute.internal>
2021-04-02 17:07:23 +0000

    follow added

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:following_id])
-    current_user.follow(@user)
+    current_user.follow(@user).save
   end
 
   def destroy
     @user = User.find(params[:id])
-    current_user.unfollow(@user)
+    current_user.unfollow(@user).destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,8 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  
+
   resources :relationships, only: [:create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
